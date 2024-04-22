9dbf50dd9a240314d9d6152af026461cd1a02037
Fredo Ronan <104075014+Fredo006@users.noreply.github.com>
2024-04-22 13:33:07 +0700

    Add files via upload

---
+++ CCTV_Service/Sleman/denggung_utara/config.js
@@ -0,0 +1,11 @@
+var playerConfig = {
+    "autoplay": false,
+    "mute": false,
+    "statistics": false,
+    "color": "#3daa48",
+    "logo": {
+        "image": "",
+        "position": "bottom-right",
+        "link": ""
+    }
+}
